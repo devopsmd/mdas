f64f908bc0c699d5b8ea60f1aaba34c8383d41b1
dmrin <dmrin@LAPTOP-NC0KKTHF>
2020-10-02 22:56:23 +0530

    hii

---
+++ src/test/java/com/nec/cucumber/TestRunner.java
@@ -0,0 +1,15 @@
+package com.nec.cucumber;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+ features = "Feature"
+ ,glue={"stepDefinition"}
+ )
+ 
+public class TestRunner {
+ 
+}
---
+++ src/test/resources/EndtoEndSearch.feature
@@ -0,0 +1,10 @@
+Feature: EndtoEnd search
+
+
+Scenario: EndtoEnd search by existing user
+
+Given User is on login page
+When User enters username
+And USer enters password
+And User clicks on sigin button
+Then User able to logged in
